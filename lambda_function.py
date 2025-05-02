ad00f188dab968bd61952cf60d09dbc03e292ef2
jalesh-0 <jalesh@xtennetworks.com>
2025-05-02 15:30:34 +0530

    test-3

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     print("Hello")
-    print("Hello")
+    print("First Line")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! , Now')
